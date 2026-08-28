c4dd8c28f3d7cc66edd06c597a133cbc459a7232
neetgill64-hub <neetgill64@gmail.com>
2026-08-28 17:08:45 +1200

    Update 04_department_analysis.sql

--- SQL/04_department_analysis.sql
+++ SQL/04_department_analysis.sql
@@ -1,1 +1,27 @@
+Which departments generate the most revenue and profit?
+SELECT
+    d.department,
+    
+    SUM(f.revenue) AS total_revenue,
+    
+    SUM(f.revenue - f.cogs - f.operating_expense) AS total_profit,
+    
+    ROUND(
+        SAFE_DIVIDE(
+            SUM(f.revenue - f.cogs - f.operating_expense),
+            SUM(f.revenue)
+        ) * 100,
+        2
+    ) AS profit_margin_percent
 
+FROM `financial-data-505202.Financial_data.fact_financials_final` f
+
+JOIN `financial-data-505202.Financial_data.dim_department` d
+    ON f.department_key = d.department_key
+
+GROUP BY
+    d.department
+
+ORDER BY
+    total_revenue DESC;
+......................................................
