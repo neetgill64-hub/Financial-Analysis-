e25acc29b48292ca89f9383fd7763ce49457c688
neetgill64-hub <neetgill64@gmail.com>
2026-08-28 17:10:15 +1200

    Add product revenue analysis with variance calculation

--- SQL/03_product_analysis.sql
+++ SQL/03_product_analysis.sql
@@ -23,3 +23,43 @@ GROUP BY
     p.Product_Category
 ORDER BY
     total_revenue DESC;
+..........................................................................
+    ## check actual revenue vs budget_revenue
+WITH product_analysis AS (
+    SELECT
+        p.Product_Category,
+
+        SUM(f.revenue) AS actual_revenue,
+
+        SUM(f.budget_revenue) AS budget_revenue,
+
+        SUM(f.revenue) - SUM(f.budget_revenue) AS variance,
+
+        ROUND(
+            SAFE_DIVIDE(
+                SUM(f.revenue) - SUM(f.budget_revenue),
+                SUM(f.budget_revenue)
+            ) * 100,
+            2
+        ) AS variance_percent
+
+    FROM `financial-data-505202.Financial_data.fact_financials_final` f
+
+    JOIN `financial-data-505202.Financial_data.dim_product` p
+        ON f.product_key = p.product_key
+
+    GROUP BY
+        p.Product_Category
+)
+
+SELECT
+    *,
+    RANK() OVER (
+        ORDER BY variance_percent DESC
+    ) AS variance_rank
+
+FROM product_analysis
+
+ORDER BY
+    variance_percent DESC;
+..............................................
