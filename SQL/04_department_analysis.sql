b412822c9918f8f9375c37cf7133485e6e1ab5eb
neetgill64-hub <neetgill64@gmail.com>
2026-08-28 17:45:22 +1200

    Update 04_department_analysis.sql

--- SQL/04_department_analysis.sql
+++ SQL/04_department_analysis.sql
@@ -25,3 +25,30 @@ GROUP BY
 ORDER BY
     total_revenue DESC;
 ......................................................
+    ### Which departments are performing above or below their revenue budget?
+SELECT
+    d.department,
+    SUM(f.revenue) AS actual_revenue,
+    SUM(f.budget_revenue) AS budget_revenue,
+    
+    SUM(f.revenue) - SUM(f.budget_revenue) AS variance,
+    
+    ROUND(
+        SAFE_DIVIDE(
+            SUM(f.revenue) - SUM(f.budget_revenue),
+            SUM(f.budget_revenue)
+        ) * 100,
+        2
+    ) AS variance_percent
+
+FROM `financial-data-505202.Financial_data.fact_financials_final` f
+
+JOIN `financial-data-505202.Financial_data.dim_department` d
+    ON f.department_key = d.department_key
+
+GROUP BY
+    d.department
+
+ORDER BY
+    variance_percent DESC;
+.........................................................................
