88eadf14d267d3c9ae10dcac9011664d9303daa1
neetgill64-hub <neetgill64@gmail.com>
2026-08-29 15:54:05 +1200

    Update 01_data_validation.sql

--- SQL/01_data_validation.sql
+++ SQL/01_data_validation.sql
@@ -5,7 +5,7 @@ SELECT
 FROM `financial-data-505202.Financial_data.fact_financials_final`
 GROUP BY transaction_id
 HAVING COUNT(*) > 1;
-.......................................................................
+.........................................................................
   ## Check NULL/missing-value validation query
 SELECT
   COUNT(*) AS total_rows,
@@ -32,7 +32,7 @@ SELECT
   operating_expense,
   revenue - cogs - operating_expense AS calculated_profit
 FROM `financial-data-505202.Financial_data.fact_financials_final`;
-.................................................................
+.......................................................................................
 
 ## Check Date Range
 
@@ -40,7 +40,7 @@ SELECT
   MIN(date_key) AS earliest_date,
   MAX(date_key) AS latest_date
 FROM `financial-data-505202.Financial_data.fact_financials_final`;
-....................................................................
+...................................................................................
   ## Check invalid financial valuesvalue
 SELECT *
 FROM `financial-data-505202.Financial_data.fact_financials_final`
@@ -48,4 +48,36 @@ WHERE revenue < 0
    OR cogs < 0
    OR operating_expense < 0
    OR budget_revenue < 0;
-.....................................................................
+..............................................................................................
+
+## Referential Integrity Check:
+  SELECT
+  COUNT(*) AS total_fact_records,
+
+  COUNTIF(d.department_key IS NULL) AS missing_department_keys,
+
+  COUNTIF(l.region_key IS NULL) AS missing_region_keys,
+
+  COUNTIF(p.product_key IS NULL) AS missing_product_keys,
+
+  COUNTIF(c.customer_key IS NULL) AS missing_customer_keys,
+
+  COUNTIF(dt.date_key IS NULL) AS missing_date_keys
+
+FROM `financial-data-505202.Financial_data.fact_financials_final` f
+
+LEFT JOIN `financial-data-505202.Financial_data.dim_department` d
+  ON f.department_key = d.department_key
+
+LEFT JOIN `financial-data-505202.Financial_data.dim_region` l
+  ON f.region_key = l.region_key
+
+LEFT JOIN `financial-data-505202.Financial_data.dim_product` p
+  ON f.product_key = p.product_key
+
+LEFT JOIN `financial-data-505202.Financial_data.dim_customer` c
+  ON f.customer_key = c.customer_key
+
+LEFT JOIN `financial-data-505202.Financial_data.dim_date` dt
+  ON f.date_key = dt.date_key;
+...........................................................................................................
